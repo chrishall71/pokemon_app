02506899b2b57ff04d9f3d5cc430381acc9a795f
chrishall71 <chrishallsrq@gmail.com>
2019-12-18 11:55:30 -0600

    Rebuilding Repostory using Jquery
    This is rebuilding the Repository.
    None of my added elements are working so I am submitting for help.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,34 @@
-var starwarsRepository = (function () {
-	var repository = [];
-	var apiUrl = 'https://swapi.co/api/people/';
-	var $modalContainer = $('#modal-container');
-} 
+let starwarsRepository = (function () {
+	let repository = [];
+	let apiUrl = 'https://swapi.co/api/people/';
+
+function add (item) {
+	repository.push(item)
+}
+
+function catchAll() {
+	return repository;
+}
+
+function addListItem (people) {
+	let starwars = $('.starwars-list');
+	//add a li and append to Ul
+	let listItem = $('<li></li>');
+	$(starwars).append(listItem);
+	//add a button and class
+	let listButton = $('<button class= "button-style"></button>');
+	$(listButton).text(people.name);
+	$(listItem).append (listButton);
+	//add Event Listner to display showDetails funcion
+	listButton.on('click', function() {
+		showDetails(people);
+	});
+}
+function showDetails(item) {
+    starwarsRepository.loadDetails(item).then(function() {
+      showModal(item);
+    });
+
 
 
 })();
