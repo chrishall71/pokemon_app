3b0e6e5e7d6a0e4a00438a1482d45704a7833ecd
chrishall71 <chrishallsrq@gmail.com>
2019-12-20 11:34:52 -0600

    Updates - button showing but not Modal
    I made some changes corrected grammer and the page with pokemons is displayed but the modal is still not popping up

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var pokemonRepository = (function() {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  var $modalContainer = $('#modal-container');
+  var modalContainer = $('#modal-container');
   
   function addListItem(pokemon) {
     var pokemonList = $('.pokemon-list'); 
@@ -25,7 +25,7 @@ var pokemonRepository = (function() {
   
    //adds new Pokemon to var repository
   function add(item) {   
-    repository.push(name);
+    repository.push(item);
   }
 
   //Function Used To Return Pokedex Object Array
@@ -35,26 +35,25 @@ var pokemonRepository = (function() {
 
   // Function to load Pokemon list from API
   function loadList() {
+    //replaced fetch with Ajax
     return $.ajax(apiUrl, {dataType: 'json'})
-      .then (funtcion (item) 
-        //replaced fetch with Ajax
-      .each(item.results, function(index, item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-      }
-        add (pokemon);
+      .then (function (item) {  
+        $.each(item.results, function(index, item) {
+          var pokemon = {
+            name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
+            detailsUrl: item.url
+        };
+          add (pokemon);
+        });
       })  
-     
-    .catch (function (e) {
-      console.error(e);
-    })
-  }
+      .catch (function (e) {
+        console.error(e);
+      });
+    }
 
   function loadDetails(item) {
     var url = item.detailsUrl;
-    return $.ajax(url)
-      .then(function(details) {
+    return $.ajax(url).then(function(details) {
         /* Replaced Fectch With Ajax*/
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
@@ -64,12 +63,13 @@ var pokemonRepository = (function() {
         document.write(error);
       });
   }
+  
   function showModal (item){
-    $modalContainer.innerHtml ='';
+    modalContainer.html ='';
     var modal = $ ('<div class="modal"></div>');
-    var closeButtonElement = $ ('<button class="modal-close"></button>');
+    var closeButtonElement = $('<button class="modal-close"></button>');
     closeButtonElement.innerText = 'Close';
-    closeButtonElement.on('click', hideModel);
+    closeButtonElement.on('click', hideModal);
 
     var nameElement = $('<h1></h1>');
       $(nameElement).text = item.name.charAt(0).toUpperCase() + item.name.slice(1);
@@ -83,33 +83,40 @@ var pokemonRepository = (function() {
     var typesElement = $('<p></p>');
     $(typesElement).text = 'Type(s): ' + item.types;
 
-    modal.appendChild(closeButtonElement);
+    $(modal).append(closeButtonElement);
+    $(modal).append(nameElement);
+    $(modal).append(heightElement);
+    $(modal).append(imageElement);
+    $(modal).append(typesElement);
+    $(modalContainer).append(modal);
+
+    /*modal.appendChild(closeButtonElement);
 		modal.appendChild(nameElement);
 		modal.appendChild(heightElement);
 		modal.appendChild(imageElement);
 		modal.appendChild(typesElement);
-    $modalContainer.appendChild(modal);
+    modalContainer.appendChild(modal);*/
     
-    $($modalContainer) =('.is-visible');
+    $(modalContainer) = ('is-visible');
   }
 
   //Funciont to close the modal
-  function hideModal () 
-  $modalContainer.classList.remove('is-visible');
-
+  function hideModal () {
+  modalContainer.removeClass('is-visible');
+  }  
   // Press escape key to close modal
   window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
       hideModal();
     }
   });
 
 // Click outside of the modal to close the modal
-$modalContainer.addEventListener('click', (e) => {
+modalContainer.on('click', function (event) {
  // Since this is also triggered when clicking INSIDE the modal
  // I only want the modal to close if the user clicks directly on the overlay
- var target = e.target;
- if (target === $modalContainer) {
+ var target = event.target;
+ if (event.target === this) {
    hideModal();
  }
 });
@@ -123,7 +130,7 @@ $modalContainer.addEventListener('click', (e) => {
    showDetails: showDetails,
    showModal: showModal,
  };
-})();
+})(); 
 
 pokemonRepository.loadList().then(function() {
  // Now the data is loaded!
